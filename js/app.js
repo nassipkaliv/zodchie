e06be066fc943f147b553ae589615e7aedfb3ab9
nassipkaliv <nassipkaliv@gmail.com>
2025-07-12 18:50:24 +0500

    update mobilka

--- js/app.js
+++ js/app.js
@@ -17,3 +17,22 @@ const burger = document.querySelector('.header-burger');
     offcanvas.classList.remove('active');
     backdrop.classList.remove('active');
   });
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".project-features__item");
+    const showMoreBtn = document.getElementById("showMoreBtn");
+    const maxVisible = 10;
+
+    // Сначала показать первые 10
+    items.forEach((item, index) => {
+      if (index < maxVisible) {
+        item.classList.add("visible");
+      }
+    });
+
+    showMoreBtn.addEventListener("click", () => {
+      items.forEach(item => item.classList.add("visible"));
+      showMoreBtn.style.display = "none";
+    });
+  });
