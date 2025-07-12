2c741abdda8438b028b6eec4daf1c9fd0f268cec
nassipkaliv <nassipkaliv@gmail.com>
2025-07-12 19:19:20 +0500

    update

--- js/app.js
+++ js/app.js
@@ -36,3 +36,43 @@ const burger = document.querySelector('.header-burger');
       showMoreBtn.style.display = "none";
     });
   });
+
+const openButtons = document.querySelectorAll("[data-target]");
+
+openButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const targetSelector = btn.getAttribute("data-target");
+    const modal = document.querySelector(targetSelector);
+    if (modal) {
+      modal.classList.add("show");
+    }
+  });
+});
+
+// Закрытие по фону
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal-overlay")) {
+    e.target.classList.remove("show");
+  }
+});
+
+// Закрытие по ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.key === "Esc") {
+    document.querySelectorAll(".modal-overlay.show").forEach((modal) => {
+      modal.classList.remove("show");
+    });
+  }
+});
+
+// Закрытие по кнопке .modal-close
+const closeButtons = document.querySelectorAll(".modal-close");
+
+closeButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal-overlay");
+    if (modal) {
+      modal.classList.remove("show");
+    }
+  });
+});
